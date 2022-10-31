708613cbacadaf12c1444378fc8b22653bedc894
Jokand <84349256+Jokand@users.noreply.github.com>
2022-10-31 04:34:52 +0300

    Авторизация, регестрация и выход из аккаунта

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegistrationRequest;
+use App\Http\Requests\AuthRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use \Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Route;
+
+class AuthController extends Controller
+{
+    /**
+     * User registration
+     * @param RegistrationRequest $request
+     * @return JsonResponse
+     */
+    public function registration(RegistrationRequest $request): JsonResponse
+    {
+        $password = Hash::make($request->password);
+        User::query()->create(['password' => $password] + $request->validated());
+        return response()->json(['success' => true,], 201);
+    }
+
+    /**
+     * User login
+     * @param RegistrationRequest $request
+     * @return array|JsonResponse
+     */
+    public function auth(AuthRequest $request)
+    {
+        if(Auth::attempt($request->validated())){
+            return [
+                'success' => true,
+                'token' => $request->user()->createToken('api')->plainTextToken,
+            ];
+        }
+        return response()->json([
+            'errors' => [
+                'email' => ['Incorrect login'],
+            ],
+        ], 422);
+    }
+
+    /**
+     * User logout
+     * @param Request $request
+     * @return Response
+     */
+    public function logout(Request $request)
+    {
+     $request->user()->currentAccessToken()->delete();
+     return response()->noContent();
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50);
+            $table->string('username', 50);
             $table->string('email', 100)->unique();
             // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
